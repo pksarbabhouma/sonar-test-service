2cb4f9838056ea7ec8fef0b4f3aabda003ed1c51
Pradip Sarbabhouma <contact.pradipks@gmail.com>
2023-08-21 02:09:59 +0100

    Sonar test - adding duplicate lines

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { Greeter } from "./Greeter";
 
 console.log(`Setting up SonarQube for javascript project - push through PR`);
+console.log(`Setting up SonarQube for javascript project - push through PR`);
+console.log(`Setting up SonarQube for javascript project - push through PR`);
 Greeter();
